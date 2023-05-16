b75631de3c40f3b677be363566f3057f664bd057
Jonathan <storfot32@gmail.com>
2023-05-16 15:07:33 +0200

    updated listview

--- app/src/main/java/com/example/listtest/MainActivity.kt
+++ app/src/main/java/com/example/listtest/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.listtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import yuku.ambilwarna.AmbilWarnaDialog
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +14,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notesListView: ListView
     private lateinit var notesAdapter: ArrayAdapter<String>
     private lateinit var notesInput: EditText
+    private lateinit var titleInput: EditText
     private lateinit var saveButton: Button
+    private lateinit var imageButton: ImageButton
+    private lateinit var sizePicker: NumberPicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,20 +26,71 @@ class MainActivity : AppCompatActivity() {
         notesListView = findViewById(R.id.notes_list)
         notesInput = findViewById(R.id.notes_input)
         saveButton  = findViewById(R.id.save_button)
+        titleInput = findViewById(R.id.titles_input)
+        imageButton = findViewById(R.id.imageButton2)
+        sizePicker = findViewById(R.id.numberPicker)
 
-        notesAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, notesList)
+        sizePicker.minValue = 12
+        sizePicker.maxValue = 30
+        sizePicker.wrapSelectorWheel = false
+
+
+        notesAdapter = object : ArrayAdapter<String>(
+            this,
+            R.layout.list_item,
+            R.id.text1,
+            notesList
+        ) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val view = super.getView(position, convertView, parent)
+                val titleTextView = view.findViewById<TextView>(R.id.text1)
+
+                val textSize = sizePicker.value.toFloat()
+                titleTextView.textSize = textSize
+
+                return view
+            }
+        }
+
+
         notesListView.adapter = notesAdapter
 
+        notesListView.setOnItemLongClickListener { _, _, position, _ ->
+            notesAdapter.remove(notesAdapter.getItem(position))
+            true
+        }
 
         saveButton.setOnClickListener {
-            val newNote = notesInput.text.toString()
-            if (newNote.isNotEmpty()) {
-                notesList.add(newNote)
+            val newNoteCard = titleInput.text.toString() + "\n\n " + notesInput.text.toString()
+            if (newNoteCard.isNotEmpty()) {
+                notesList.add(newNoteCard)
                 notesAdapter.notifyDataSetChanged()
+                titleInput.text.clear()
                 notesInput.text.clear()
             }
-
-
         }
+
+        imageButton.setOnClickListener{
+            val textSize = sizePicker.value.toFloat()
+            titleInput.textSize = textSize
+            notesInput.textSize = textSize
+        }
+
+
+    }
+    fun changeColor(view: View){
+        val initialColor = titleInput.currentTextColor
+
+        val colorPickerDialog = AmbilWarnaDialog(this, initialColor, object : AmbilWarnaDialog.OnAmbilWarnaListener {
+            override fun onCancel(dialog: AmbilWarnaDialog?) {
+                // Handle dialog cancellation
+            }
+
+            override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+                titleInput.setTextColor(color)
+                notesInput.setTextColor(color)
+            }
+        })
+        colorPickerDialog.show()
     }
 }
