0b2c930b0dd25094bb84667ff9cf164882063955
Jonathan <storfot32@gmail.com>
2023-05-22 14:06:25 +0200

    updated listview again again

--- app/src/main/java/com/example/listtest/MainActivity.kt
+++ app/src/main/java/com/example/listtest/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.listtest
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var saveButton: Button
     private lateinit var imageButton: ImageView
     private lateinit var sizePicker: NumberPicker
+    private val EDIT_NOTE_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +63,23 @@ class MainActivity : AppCompatActivity() {
 
 
         notesListView.adapter = notesAdapter
+
+        notesListView.setOnItemClickListener { _, _, position, _ ->
+            val selectedNote = notesAdapter.getItem(position)
+            val titleEndIndex = selectedNote?.indexOf("\n")
+            val title = selectedNote?.substring(0, titleEndIndex!!)
+            val note = selectedNote?.substring(titleEndIndex!! + 1)
+            val color = textColorList[position]
+            val size = textSizeList[position]
+
+            val intent = Intent(this, EditNoteActivity::class.java)
+            intent.putExtra("position", position)
+            intent.putExtra("title", title)
+            intent.putExtra("note", note)
+            intent.putExtra("color", color)
+            intent.putExtra("size", size)
+            startActivityForResult(intent, EDIT_NOTE_REQUEST_CODE)
+        }
 
         notesListView.setOnItemLongClickListener { _, _, position, _ ->
             notesAdapter.remove(notesAdapter.getItem(position))
@@ -104,5 +124,22 @@ class MainActivity : AppCompatActivity() {
             }
         })
         colorPickerDialog.show()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == EDIT_NOTE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val position = data?.getIntExtra("position", -1)
+            val modifiedTitle = data?.getStringExtra("modifiedTitle")
+            val modifiedNote = data?.getStringExtra("modifiedNote")
+            val modifiedColor = data?.getIntExtra("modifiedColor", -1)
+            val modifiedSize = data?.getFloatExtra("modifiedSize", -16f)
+
+            if (position != -1 && modifiedTitle != null && modifiedNote != null && modifiedColor!=-1 && modifiedSize!=-16f) {
+                notesList[position!!] = (modifiedTitle + "\n" + modifiedNote)
+                textColorList[position] = modifiedColor!!
+                textSizeList[position] = modifiedSize!!
+                notesAdapter.notifyDataSetChanged()
+            }
+        }
     }
 }
